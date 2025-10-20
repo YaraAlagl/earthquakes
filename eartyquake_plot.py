c1d511a9728193804383494c54ce5b490c18a8ff
YaraAlagl <alagl.yara@gmail.com>
2025-10-20 10:01:58 +0100

    Retrieved data

--- eartyquake_plot.py
+++ eartyquake_plot.py
@@ -5,8 +5,18 @@ import matplotlib.pyplot as plt
 
 def get_data():
     """Retrieve the data we will be working with."""
-    ...
-
+   response = requests.get(
+        "http://earthquake.usgs.gov/fdsnws/event/1/query.geojson",
+        params={
+            'starttime': "2000-01-01",
+            "maxlatitude": "58.723",
+            "minlatitude": "50.008",
+            "maxlongitude": "1.67",
+            "minlongitude": "-9.756",
+            "minmagnitude": "1",
+            "endtime": "2018-10-11",
+            "orderby": "time-asc"}
+   )
 
 def get_year(earthquake):
     """Extract the year in which an earthquake happened."""
